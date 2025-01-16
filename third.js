78c188e7186c88f940bf78c1c1cf08a5961780a5
Shofikul2024 <islamshofi942@gmail.com>
2025-01-16 23:55:08 +0600

    second commit

---
+++ third.js
@@ -0,0 +1,12 @@
+   let rahim = {
+
+    Name: ' rahim   rahman' ,
+    age:  20  ,
+    address: ' jamalpur ' ,
+
+    occupation: ' students'  ,
+    salary:  1000  , 
+}
+
+console.log( rahim)
+console.log(rahim.address)
