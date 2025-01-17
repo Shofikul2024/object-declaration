8c11d475305bdcc5d0515e65db247c86a02c8a1a
Shofikul2024 <islamshofi942@gmail.com>
2025-01-17 11:22:18 +0600

    7 commit

---
+++ six.js
@@ -0,0 +1,12 @@
+const person = {
+    name: 'sodor uddin', 
+    age: 25,
+    profession: 'developer',
+    salary: 25000,
+    married: true,
+    'fav places': ['bandorban', 'saintmartin', 'kuakata']
+}
+// console.log(person['age'])
+const boyos = person['age']
+console.log(boyos)
+    
